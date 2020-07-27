6ed4263cd53482c621a78029c39e49b3260ae105
ziho9593 <62165123+ziho9593@users.noreply.github.com>
2020-07-27 20:25:08 +0900

    7월 27일 얼고리즘-Deque 예제 파일 업로드

---
+++ Week_4/07.27-Deque/stack_by_class_and_deque.py
@@ -0,0 +1,50 @@
+# import sys
+# input = sys.stdin.readline
+from collections import deque
+ 
+class Stack:
+    def __init__(self):
+        self.array_list = deque()
+        self.cnt = 0
+    
+    def push(self, value):
+        self.array_list.append(value)
+        self.cnt += 1
+    
+    def pop(self):
+        if self.cnt > 0:
+            self.cnt -= 1
+            return self.array_list.pop()
+        
+        return -1
+    
+    def size(self):
+        return self.cnt
+    
+    def empty(self):
+        return int(self.cnt <= 0)
+    
+    def top(self):
+        if self.cnt > 0:
+            return self.array_list[self.cnt-1]        
+        return -1
+
+def process_stack(stack_list, command):
+    cmd = command[0]
+    if cmd == "push":
+        stack_list.push(command[1])
+    elif cmd == "pop":
+        print(stack_list.pop())
+    elif cmd == "size":
+        print(stack_list.size())
+    elif cmd == "empty":
+        print(stack_list.empty())
+    elif cmd == "top":
+        print(stack_list.top())
+ 
+n = int(input())
+stack_list = Stack()
+
+for _ in range(n):
+    command = input().split()
+    process_stack(stack_list, command)
