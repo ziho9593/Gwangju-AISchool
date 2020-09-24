49046edfc6399f5eeb069dad43ad54134f81ec3d
Jiho <no1hjyo@naver.com>
2020-09-25 03:34:45 +0900

    유사도 측정 함수 및 views.py의 recipe 함수 수정

---
+++ HackathonProject/HackathonApp/modules/check.py
@@ -0,0 +1,45 @@
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+def data_to_list(data):
+    result = []
+
+    for d in data:
+        for key in d:
+            temp = []
+            for w in d[key]:
+                temp.append(w[0])
+            result.append(temp)
+
+    for i in range(len(result)):
+        temp = ''
+        for j in result[i]:
+            temp += j.replace(' ', '') + ' '
+        result[i] = temp[:-1]
+
+    return result
+
+
+def similiar_check(data, user):
+    m_list = data_to_list(data)
+    m_list.append(user)
+    
+    vec = CountVectorizer(ngram_range=(1, 2))
+    matrix = vec.fit_transform(m_list)
+
+    similarity = cosine_similarity(matrix, matrix)
+
+    count_similar_index = np.argsort(-similarity)
+
+    similar = count_similar_index[-1, 1:4]
+
+    similar_index = similar.reshape(-1)
+
+    result = []
+    for i in similar_index:
+        result += list(data[i].keys())
+    
+    return result[0], result[1], result[2]
--- HackathonProject/HackathonApp/views.py
+++ HackathonProject/HackathonApp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .modules.check import similiar_check
+import json
 
 # Create your views here.
 
@@ -7,6 +9,10 @@ def home(request):
     return render(request, 'home.html')
 
 def recipe(request):
+    file_path = './HackathonApp/data/material_list.json'
+    with open(file_path, "r") as json_file:
+        data = json.load(json_file)
+
     food1 = request.POST["food1"]
     food2 = request.POST["food2"]
     food3 = request.POST["food3"]
@@ -16,20 +22,22 @@ def recipe(request):
     food7 = request.POST["food7"]
     food8 = request.POST["food8"]
     food9 = request.POST["food9"]
+
+    user = [food1, food2, food3, food4, food5, food6, food7, food8, food9]
+    temp = ''
+    for u in user:
+        temp += u + ' '
+    temp = temp.rstrip()
+    result1, result2, result3 = similiar_check(data, temp)
+
     context = {
-        "food1": food1,
-        "food2": food2,
-        "food3": food3,
-        "food4": food4,
-        "food5": food5,
-        "food6": food6,
-        "food7": food7,
-        "food8": food8,
-        "food9": food9,
-        "recipe1": "김치전",
+        'result1': result1,
+        'result2': result2,
+        'result3': result3,
     }
-    
+
     return render(request, 'recipe.html', context)
+
 
 def recipe1(request):
     context = {
@@ -37,6 +45,7 @@ def recipe1(request):
     }
     
     return render(request, 'recipe1.html', context)
+
 
 def recipe2(request):
     
